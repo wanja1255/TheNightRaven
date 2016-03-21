3d2e2d37cad60e37aa3761a9bb828f26b465afaa
Jack White <nightraven754@gmail.com>
2016-03-21 16:15:02 +0300

    Add vision status code

--- src/main/java/xyz/zaddrot/main/Starter.java
+++ src/main/java/xyz/zaddrot/main/Starter.java
@@ -13,6 +13,7 @@ public class Starter {
         try {
             Connection.Response SMS = Jsoup.connect("http://sms.ru/sms/send?api_id=8CF99BD9-9565-171F-0D5B-8F1D88CF3428&to=79807057233&text=hello+world").method(Connection.Method.GET).execute();
             System.out.println(SMS.body());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
